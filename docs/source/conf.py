457cbfc0bf3bfa88a724dec3e2da8d1a22808603
volodymyr.kovenko <volodymyr.kovenko@vimmi.net>
2020-01-03 13:53:28 +0200

    c

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,12 +53,8 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
+html_title = 'DS/ML course'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-man_pages = [
-    (master_doc, 'implicit', u'Implicit Documentation',
-     [author], 1)
-]
