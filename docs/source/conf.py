ebaff3e8d7c8164113a59789a04b3d08d85740bf
volodymyr.kovenko <volodymyr.kovenko@vimmi.net>
2020-01-03 14:04:39 +0200

    c

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,3 +58,20 @@ html_title = 'DS/ML course'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': '', #  Provided by Google in your dashboard
+    'logo_only': True
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
